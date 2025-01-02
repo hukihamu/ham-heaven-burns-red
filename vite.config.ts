f35ffdc2403a0e823cbbcd120978de710c8dc865
hukihamu <8084194+hukihamu@users.noreply.github.com>
2025-01-02 14:24:41 +0900

    init commits

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/ham-heaven-burns-red/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
